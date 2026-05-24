cc9fc013ca4d701b400964d526c3a416145f731b
Michael Fischer <mfischer1403@outlook.de>
2026-05-24 10:54:27 +0200

    Fix cloud class loading order

--- js/classes/background-object.class.js
+++ js/classes/background-object.class.js
@@ -8,3 +8,16 @@ class BackgroundObject extends DrawableObject {
         this.height = 480;
     }
 }
+
+class Cloud extends BackgroundObject {
+    speed = 0.2;
+
+    /**
+     * Moves the cloud layer slowly from right to left.
+     * @param {number} levelEndX Last world x-position.
+     */
+    update(levelEndX) {
+        this.x -= this.speed;
+        if (this.x <= -this.width) this.x = levelEndX + this.width;
+    }
+}
